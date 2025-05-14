ba80cb408db2940f2c194addc10b16794f708b5a
Bernard Bisuna <bernard_bisuna@trendmicro.com>
2025-05-14 12:34:10 -0700

    removed non-working google tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,6 @@
 // test_main.cpp
 #include <gtest/gtest.h>
+//#include "utility.h"
 
 TEST(SampleTest, BasicAssertion) {
     EXPECT_EQ(1, 1);
@@ -9,3 +10,10 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+// TEST(GreedyVacationRouteTest, Simple3StopPath) {
+//     std::vector<std::string> cities = {"A", "B", "C"};
+
+//     GraphDijkstra graph(cities);
+
+// }
